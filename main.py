31434cefb7edefc09dab1fedb0177a236a41d5e6
Clara Cousteix <clara.cousteix@student-cs.fr>
2023-03-29 10:01:11 +0200

    some changes + newnet is running with images only

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 from dataset import niiDataset
-from model import FirstNet, CNN, Loop, CNN_tho
+from model import FirstNet, CNN, Loop, CNN_tho, NewNet
 from os.path import join
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
 
 
 if __name__ == "__main__":
 
-    ### CONFIG
+    ### WHO IS RUNNING THE CODE
+    on_ruche = False
+    user = "cousteixc"
 
-    print("coucouu")
-    path_to_dir = "/gpfs/workdir/menardth/dose_matrices"
+    ### CONFIG
+    if on_ruche:
+        path_to_dir = f"/gpfs/workdir/{user}/dose_matrices"
+        csv_name = "labels_ruche.csv"
+    else : 
+        csv_name = "labels.csv"
+        if user=="cousteixc":
+            path_to_dir = "D:\data\dose_matrices_updated_25_01_2023"
+
     min_card_age = 50
-    batch_size = 2
+    batch_size = 4
     epochs = 50
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     
     ### DATALOADER
-    training_data = niiDataset("labels_ruche.csv", path_to_dir, min_card_age, training = True)
-    val_data = niiDataset("labels_ruche.csv", path_to_dir, min_card_age, validation = True)
+    training_data = niiDataset(csv_name, path_to_dir, min_card_age, training = True)
+    val_data = niiDataset(csv_name, path_to_dir, min_card_age, validation = True)
 
     print(len(training_data), len(val_data))
 
@@ -27,17 +38,15 @@ if __name__ == "__main__":
     val_dataloader = DataLoader(val_data, batch_size, shuffle=True, drop_last=True)
 
     ### NET AND LOSS
-    #net = NewNet(15, 10)
-    net = CNN(16).to(DEVICE)
-    net = CNN_tho(16).to(DEVICE)
+    net = NewNet(15, 10).to(DEVICE)
     loss = nn.CrossEntropyLoss()
     optimizer_adam = torch.optim.Adam(params=net.parameters(), lr=0.001)
-    model = Loop(train_dataloader, val_dataloader, net, loss, optimizer_adam, DEVICE)
+    model = Loop(train_dataloader, val_dataloader, net, loss, optimizer_adam, DEVICE, join(path_to_dir, "saved_models", "new_net_0.pth"))
     print(f"Training for {epochs} epochs on {DEVICE}")
 
-    for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        model.train_loop()
-        model.validation_loop()
+    for epoch in range(epochs):
+        print(f"Epoch {epoch+1}\n-------------------------------")
+        model.train_loop(epoch)
+        model.validation_loop(epoch)
     print("Done!")    
 
--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch
 from sklearn.metrics import balanced_accuracy_score, cohen_kappa_score, confusion_matrix,f1_score, ConfusionMatrixDisplay
 import os
+from tqdm import tqdm 
 
 
 
@@ -56,48 +57,47 @@ class CNN(nn.Module):
         return x
 
 
-"""class NewNet():
-    super(NewNet, self).__init__()
+class NewNet(nn.Module):
+     
+     def __init__(self, out_channels_1: int, out_channels_2: int):
+        super(NewNet, self).__init__()
+
         self.out_channels_1 : int = out_channels_1
         self.out_channels_2 : int = out_channels_2
 
         self.conv1 = nn.Sequential(
             nn.Conv3d(1, self.out_channels_1, kernel_size=3, stride = 1, padding = 'valid'),
-            nn.ReLU()
+            nn.ReLU(),
             nn.Dropout(),
             nn.MaxPool3d(kernel_size=3)
             )
         self.conv2 = nn.Sequential(
             nn.Conv3d(self.out_channels_1, self.out_channels_2, kernel_size=2, stride = 1, padding = 'valid'),
-            nn.ReLU()
+            nn.ReLU(),
             nn.Dropout(),
             nn.MaxPool3d(kernel_size=3)
             )
         self.conv3 = nn.Sequential(
             nn.Conv3d(self.out_channels_2, self.out_channels_1, kernel_size=2, stride = 1, padding = 'valid'),
-            nn.ReLU()
+            nn.ReLU(),
             nn.Dropout(),
             nn.MaxPool3d(kernel_size=3)
             )
 
-        self.fc1 = nn.Linear(self.out_channels_2*64*64*64, hidden_units)
-        self.fc2 = nn.Linear(hidden_units, hidden_units)
-        self.fc3 = nn.Linear(hidden_units, 2)
+        self.fc1 = nn.Linear(self.out_channels_1, 10)
+        self.fc2 = nn.Linear(10, 2)
 
 
-    def forward(self, x):
+     def forward(self, x):
         x = x.float()
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        print(x.shape)
-        x = x.view(-1, self.out_channels*64*64*64)
+        x = x.view(-1, self.out_channels_1)
         x = self.fc1(x)
         x = nn.ReLU()(x)
         x = self.fc2(x)
-        x = nn.ReLU()(x)
-        x = self.fc3(x)
-        return x"""
+        return x
 
 class CNN_tho(nn.Module):
     def __init__(self, out_channels: int, hidden_units : int =200):
@@ -159,23 +159,23 @@ class CNN_tho(nn.Module):
 
 class Loop():
 
-    def __init__(self, train_dataloader, val_dataloader, net, loss_fct, optimizer, device):
+    def __init__(self, train_dataloader, val_dataloader, net, loss_fct, optimizer, device, path_to_saved_models):
         self.train_dataloader = train_dataloader
         self.val_dataloader = val_dataloader
         self.net = net
         self.loss_fct = loss_fct
         self.optimizer = optimizer
         self.device= device
-        self.save_best_model = SaveBestModel()
+        self.save_best_model = SaveBestModel(path_to_saved_models)
 
-    def train_loop(self):
+    def train_loop(self, epoch):
         size = len(self.train_dataloader.dataset)
         num_batches = len(self.val_dataloader)
         train_loss, train_acc = 0, 0
 
-        for batch, (X, y) in enumerate(self.train_dataloader):
+        for X, y in tqdm(self.train_dataloader, desc=f"Epoch {epoch+1} training...", ascii=False, ncols=75, leave=False):
             # Compute prediction and loss
-            pred = self.net(X.float().to(self.device))   #pas sûre que ça fasse un forward pass
+            pred = self.net(X.float().to(self.device))  
             loss = self.loss_fct(pred, y.to(self.device)) #pred.float() ?
 
             # Backpropagation
@@ -183,43 +183,40 @@ class Loop():
             loss.backward()
             self.optimizer.step()
 
-            loss += loss.item()/num_batches
-            y_pred_class = pred.argmax()
-            train_acc += (y_pred_class.to(self.device) == y.to(self.device)).sum()/num_batches
+            train_loss += loss.item()/num_batches
+            y_pred_class = pred.argmax(1)
+            train_acc += (y_pred_class.to(self.device) == y.to(self.device)).sum()/size
 
-        print(f"Train Error: \n Accuracy: {100*train_acc}%, Avg loss: {loss:>8f} \n")
+        print(f"Train Error: \n Accuracy: {100*train_acc}%, Avg loss: {train_loss:>8f} \n")
 
-    def validation_loop(self):
+    def validation_loop(self, epoch):
         size = len(self.val_dataloader.dataset)
         num_batches = len(self.val_dataloader)
         test_loss, correct = 0, 0
         y_val, y_pred = [], []
 
         with torch.no_grad():
-            for X, y in self.val_dataloader:
+            for X, y in tqdm(self.val_dataloader, desc=f"Epoch {epoch+1} testing...", ascii=False, ncols=75, leave=False):
                 y_val.extend(y.tolist())
                 pred = self.net(X.float().to(self.device))
                 y_pred.extend(pred.argmax(1).tolist())
-                test_loss += self.loss_fct(pred, y.to(self.device)).item()
-                correct += (pred.argmax(1).to(self.device) == y.to(self.device)).type(torch.float).sum().item()
+                test_loss += self.loss_fct(pred, y.to(self.device)).item()/num_batches
+                correct += (pred.argmax(1).to(self.device) == y.to(self.device)).type(torch.float).sum().item()/size
 
         #print(f"pred = {y_pred}")
         #print(f"true = {y_val}")
 
-
-        test_loss /= num_batches
-        correct /= size
         print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
         scores = {'balanced_accuracy': balanced_accuracy_score(y_val, y_pred),
                 'cohen_kappa': cohen_kappa_score(y_val, y_pred),
                 'macro_f1': f1_score(y_val, y_pred,average ='macro')}
         print(scores)
 
-        self.save_best_model(test_loss, self.net, "model_cnn_16.pth")
+        self.save_best_model(test_loss, self.net)
 
-        #cm = confusion_matrix(y_true=y_val, y_pred=y_pred)
-        #disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-        #disp.plot()
+        cm = confusion_matrix(y_true=y_val, y_pred=y_pred)
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+        disp.plot()
 
 
 class SaveBestModel:
@@ -228,14 +225,13 @@ class SaveBestModel:
     validation loss is less than the previous least less, then save the
     model state.
     """
-    def __init__(
-        self, best_valid_loss=float('inf')
-    ):
+    def __init__(self, path_to_saved_models, best_valid_loss=float('inf')):
         self.best_valid_loss = best_valid_loss
+        self.path_to_saved_models = path_to_saved_models
         
-    def __call__(self, current_valid_loss, model, name_model):
+    def __call__(self, current_valid_loss, model):
         if current_valid_loss < self.best_valid_loss:
             self.best_valid_loss = current_valid_loss
             print(f"\nBest validation loss: {self.best_valid_loss}")
             print(f"\nSaving best model\n")
-            torch.save(model.state_dict(), os.path.join('/gpfs/workdir/cousteixc/dose_matrices/saved_models', name_model))
+            torch.save(model.state_dict(), self.path_to_saved_models)
